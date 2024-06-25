57dacd51b73c969e6abca2949d8c4b7122b26f70
Severiano Sisneros <seve.sisneros@protonmail.com>
2024-06-25 17:12:54 -0500

    docs: Cleaned up documentation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use alloy_network::EthereumWallet;
 use alloy_primitives::{address, Bytes, FixedBytes};
 use alloy_provider::ProviderBuilder;
-use alloy_sol_types::sol;
 use alloy_signer_local::PrivateKeySigner;
-use serde::{Serialize, Deserialize};
-use std::str::FromStr;
+use alloy_sol_types::sol;
+use serde::{Deserialize, Serialize};
 use std::env;
+use std::str::FromStr;
 
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -15,9 +15,8 @@ struct ProofFixture {
 }
 
 sol! {
-    #[sol(rpc)] // <-- Important! Generates the necessary `MyContract` struct and function methods.
-    #[sol(bytecode = "0x1234")] // <-- Generates the `BYTECODE` static and the `deploy` method.
-    contract MyContract {
+    #[sol(rpc)] // <-- Important! Generates the necessary `SnarkBasedFeeOracle` struct and function methods.
+    contract SnarkBasedFeeOracle {
         constructor(address) {} // The `deploy` method will also include any constructor arguments.
 
         #[derive(Debug)]
@@ -26,33 +25,40 @@ sol! {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>>{
-
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Need a private key for signing the transaction
     let private_key = env::var("PRIVATE_KEY")?;
     let drpc_key = env::var("DRPC_KEY")?;
-    let drpc_url = format!("https://lb.drpc.org/ogrpc?network=sepolia&dkey={}", drpc_key);
+    let drpc_url = format!(
+        "https://lb.drpc.org/ogrpc?network=sepolia&dkey={}",
+        drpc_key
+    );
     let signer = PrivateKeySigner::from_bytes(&FixedBytes::from_str(&private_key)?)?;
     let wallet = EthereumWallet::new(signer);
+
     // Build a provider.
-    let provider = ProviderBuilder::new().with_recommended_fillers().wallet(wallet).on_builtin(&drpc_url).await?;
+    let provider = ProviderBuilder::new()
+        .with_recommended_fillers()
+        .wallet(wallet)
+        .on_builtin(&drpc_url)
+        .await?;
 
-
-    // Otherwise, or if already deployed, a new contract instance can be created with `MyContract::new`.
+    // Create a new contract instance can be created with `SnarkBasedFeeOracle::new`.
     let address = address!("6Ae1e19F65b474B7Eff9A22F33cc72611b0FC24A");
-    let contract = MyContract::new(address, &provider);
+    let contract = SnarkBasedFeeOracle::new(address, &provider);
 
-    // Build a call to the `doStuff` function and configure it.
-    let proof_string= std::fs::read_to_string("./src/fixture.json")?;
+    // Build a call to the `verifyAndUpdate` function and configure it.
+    let proof_string = std::fs::read_to_string("./src/fixture.json")?;
     let proof_fixture: ProofFixture = serde_json::from_str(&proof_string)?;
-    let proof = proof_fixture.proof; 
-    let public_values = proof_fixture.public_values; 
+    let proof = proof_fixture.proof;
+    let public_values = proof_fixture.public_values;
     let call_builder = contract.verifyAndUpdate(proof, public_values);
 
     // Send the call. Note that this is not broadcasted as a transaction.
     let call_return = call_builder.call().await?;
-    println!("{call_return:?}"); // doStuffReturn { c: 0x..., d: 0x... }
+    println!("{call_return:?}"); // verifyAndUpdateReturn
 
     // Use `send` to broadcast the call as a transaction.
     let _pending_tx = call_builder.send().await?;
     Ok(())
- }
+}
